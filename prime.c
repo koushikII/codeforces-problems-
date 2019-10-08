df137dfdda2a3c0a887a60042e89bfbf6d20b2cc
koushikII <46400011+koushikII@users.noreply.github.com>
2019-10-09 00:08:35 +0530

    Add files via upload

---
+++ prime.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(void) {
+	int T ;
+	scanf("%d",&T);
+	while (T--)
+	{ long long int x , y;
+	scanf("%lld%lld",&x,&y);
+	long long int z;
+	z = x - y ;
+	if (z==1)
+	  {printf("NO\n") ;}
+	else
+	   {printf("YES\n") ;}
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+	return 0;
+}
